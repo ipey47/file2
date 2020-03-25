7ec49f5cac32bac4892be8096df661245e09e2ba
ipey47 <62617498+ipey47@users.noreply.github.com>
2020-03-25 20:14:32 +0700

    Add files via upload

---
+++ a2.sh
@@ -0,0 +1,34 @@
+ # 10 AKUN AKTIF 
+      
+      python main.py +17014197218 doge&&
+      python main.py +12817836199 doge&&
+      python main.py +17014197142 doge&&
+      python main.py +17014197218 zec&&
+      python main.py +12817836199 zec&&
+      python main.py +17014197142 zec&&
+      python main.py +17014197218 ltc&&
+      python main.py +12817836199 ltc&&
+      python main.py +17014197142 ltc&&
+      python main.py +17014197116 doge&&
+      python main.py +17313268885 doge&&
+      python main.py +17014304143 doge&&
+      python main.py +17014197116 zec&&
+      python main.py +17313268885 zec&&
+      python main.py +17014304143 zec&&
+      python main.py +17014197116 ltc&&
+      python main.py +17313268885 ltc&&
+      python main.py +17014304143 ltc&&
+      python main.py +17014304221 doge&&
+      python main.py +17014304231 doge&&
+      python main.py +17014304429 doge&&
+      python main.py +17014304355 doge&&
+      python main.py +17014304221 zec&&
+      python main.py +17014304231 zec&&
+      python main.py +17014304429 zec&&
+      python main.py +17014304355 zec&&
+      python main.py +17014304221 ltc&&
+      python main.py +17014304231 ltc&&
+      python main.py +17014304429 ltc&&
+      python main.py +17014304355 ltc&&
+      sleep 7200
+      sh bot.sh
